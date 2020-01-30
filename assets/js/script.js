e57899fd6c29ada76a2e7b5ea48810b2037a0adf
Giovanni <g.l.mierop@gmail.com>
2020-01-30 16:37:01 +0100

    Goed als klaar

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,24 @@
 //         menu.style.paddingTop = 0 + "px";
 //     }
 // }
+
+function dropDownHeader() {
+    document.getElementById("dropdown-header").classList.toggle("show");
+}
+
+function dropDownNav() {
+    document.getElementById("dropdown-nav").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
